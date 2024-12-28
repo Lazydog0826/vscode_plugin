d6e9a660a161d786f057ab16e4a6969bae408106
hzc <huangzicheng@wexsu.com>
2024-12-28 15:43:28 +0800

    feat: 没行消息去重，使用code判断多次点击始终只显示最后一次翻译

--- error-translate/src/extension.ts
+++ error-translate/src/extension.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import { md5 } from "js-md5";
 
 let arr: Array<MessageModel> = [];
+let code = "";
 
 // 获取插件设置
 const config = vscode.workspace.getConfiguration("ErrorTranslate");
@@ -35,6 +36,8 @@ export function activate(context: vscode.ExtensionContext) {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
       const document = editor.document;
+      const temCode = Math.random().toString().replace(".", "");
+      code = temCode;
 
       // 先清除旧的
       arr.forEach((e) => {
@@ -49,53 +52,55 @@ export function activate(context: vscode.ExtensionContext) {
             x.severity === vscode.DiagnosticSeverity.Warning
         )
         .forEach((x) => {
-          x.message
-            .split("\n")
-            .filter((x2) => x2.trim() !== "")
-            .forEach((x2) => {
-              arr.push({
-                src: x2,
-                dst: "",
-                start: x.range.start,
-                end: x.range.end,
-                decorationType: undefined,
-                type: x.severity,
-              });
+          let splitArr = x.message.split("\n").filter((x2) => x2.trim() !== "");
+          splitArr = [...new Set(splitArr)];
+          splitArr.forEach((x2) => {
+            arr.push({
+              src: x2,
+              dst: "",
+              start: x.range.start,
+              end: x.range.end,
+              decorationType: undefined,
+              type: x.severity,
             });
+          });
         });
       translate(arr.map((x) => x.src)).then((r) => {
-        r.data.trans_result.forEach((x) => {
-          arr
-            .filter((x2) => x2.src === x.src)
-            .forEach((x2) => {
-              x2.dst = x.dst;
-              x2.decorationType = vscode.window.createTextEditorDecorationType({
-                isWholeLine: true,
-                after: {
-                  contentText: x2.dst,
-                  color:
-                    x2.type === vscode.DiagnosticSeverity.Error
-                      ? "rgba(235,47,6,1)"
-                      : "rgba(250,152,58,1)",
-                  backgroundColor:
-                    x2.type === vscode.DiagnosticSeverity.Error
-                      ? "rgba(235,47,6,0.2)"
-                      : "rgba(250,152,58,0.2)",
-                  margin: "0 5px 0 5px",
-                },
+        if (temCode === code) {
+          r.data.trans_result.forEach((x) => {
+            arr
+              .filter((x2) => x2.src === x.src)
+              .forEach((x2) => {
+                x2.dst = x.dst;
+                x2.decorationType =
+                  vscode.window.createTextEditorDecorationType({
+                    isWholeLine: true,
+                    after: {
+                      contentText: x2.dst,
+                      color:
+                        x2.type === vscode.DiagnosticSeverity.Error
+                          ? "rgba(235,47,6,1)"
+                          : "rgba(250,152,58,1)",
+                      backgroundColor:
+                        x2.type === vscode.DiagnosticSeverity.Error
+                          ? "rgba(235,47,6,0.2)"
+                          : "rgba(250,152,58,0.2)",
+                      margin: "0 5px 0 5px",
+                    },
+                  });
               });
-            });
-        });
-        arr.forEach((x) => {
-          const errors: vscode.DecorationOptions[] = [
-            {
-              range: new vscode.Range(x.start, x.end),
-            },
-          ];
-          if (x.decorationType) {
-            editor?.setDecorations(x.decorationType, errors);
-          }
-        });
+          });
+          arr.forEach((x) => {
+            const errors: vscode.DecorationOptions[] = [
+              {
+                range: new vscode.Range(x.start, x.end),
+              },
+            ];
+            if (x.decorationType) {
+              editor?.setDecorations(x.decorationType, errors);
+            }
+          });
+        }
       });
     }
   });
